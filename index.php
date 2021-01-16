f267247157e17477352c7de225a12492174d8a29
Ohashi_uhbie <tmatomatox1@gmail.com>
2021-01-16 23:00:37 +0900

    add sns at index.php

--- index.php
+++ index.php
@@ -10,6 +10,16 @@
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <meta name="description" content="かんたんにアップロード、リンクを取得できます。セキュリティはゼロ。">
+    <meta property="og:title" content="Markdown Parking">
+    <meta property="og:description" content="かんたんにアップロード、リンクを取得できます。セキュリティはゼロ。">
+    <meta property="og:url" content="https://ketcha.xyz/pr/md/">
+    <meta property="og:image" content="https://ketcha.xyz/pr/md/bela_totte/1/watermark.jp">
+    <meta property="og:type" content="article">
+    <meta property="og:locale" content="ja_JP">
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:site" content="oha_oha_Ohashi">
+
     <!--bootstrap-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
